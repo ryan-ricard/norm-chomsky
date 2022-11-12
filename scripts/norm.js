b2ae254e213dc7fd3cf7b80401dd22874e99e30f
Ryan Ricard <wally@firewally.net>
2022-11-12 09:29:27 -0600

    rotate photos

--- scripts/norm.js
+++ scripts/norm.js
@@ -5,8 +5,17 @@ const assignQuote = (quotes) => {
     document.getElementById("quote").innerHTML = randomQuote.replaceAll('\n','<br />');
 }
 
+const assignPhoto = () => {
+    const number = Math.floor(Math.random() * 10) + 1;
+    const photoID = number.toString().padStart(3, '0');
+    const url = `/img/noam/${photoID}.jpg`
+    document.getElementById('photo').src=url;
+}
+
 document.addEventListener("DOMContentLoaded", function() {
     fetch('data/norm-quotes.json')
       .then(res => res.json()) // the .json() method parses the JSON response into a JS object literal
       .then(assignQuote);
+
+    assignPhoto();
 });
