ba087fb1dc65073687a974b4fd9a846313afb453
Ryan Ricard <wally@firewally.net>
2022-11-12 14:38:41 -0600

    layout changes, quote rotation

--- scripts/norm.js
+++ scripts/norm.js
@@ -3,19 +3,28 @@
 const assignQuote = (quotes) => {
     const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
     document.getElementById("quote").innerHTML = randomQuote.replaceAll('\n','<br />');
+    document.getElementById("quote").style.display = 'block';
 }
 
-const assignPhoto = () => {
+const assignPhoto = (photoDir) => {
     const number = Math.floor(Math.random() * 10) + 1;
     const photoID = number.toString().padStart(3, '0');
-    const url = `/img/noam/${photoID}.jpg`
-    document.getElementById('photo').src=url;
+    const url = `/img/${photoDir}/${photoID}.jpg`
+    const img = document.getElementById('photo')
+    img.src=url;
+    if (photoDir === 'norm'){
+        img.alt='Photo of Norm Peterson, played by George Wendt'
+    }
+    img.style.display = 'block';
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-    fetch('data/norm-quotes.json')
+    const MODE = Math.random() < 0.5 ? "NORM" : "NOAM";
+    const quoteURL = MODE === "NORM" ? 'data/norm-quotes.json' : 'data/noam-quotes.json';
+    fetch(quoteURL)
       .then(res => res.json()) // the .json() method parses the JSON response into a JS object literal
       .then(assignQuote);
 
-    assignPhoto();
+    const photoDir = MODE === "NORM" ? "noam" : 'norm';
+    assignPhoto(photoDir);
 });
